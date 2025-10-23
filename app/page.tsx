8cdc7c7ddf8f271318a9d5a5bab457489558148b
Aroobs-i <iftikhar8995@gmail.com>
2025-10-23 18:43:56 +0500

    implement Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,50 @@
+import ComapnionLists from "@/components/ComapnionLists"
+import CompanionCard from "@/components/CompanionCard"
+import CTA from "@/components/CTA"
+import { recentSessions } from "@/constants"
 
 const Page = () => {
   return (
-    <div>Page</div>
+    <main>
+      <h1>Popular Comapnions</h1>
+      
+      <section className="home-section">
+        <CompanionCard
+           id="123"
+           name="Neura the Brainy Explorer"
+           topic="Neural Network of the Brain"
+           subject="science"
+           duration={45}
+           color="#ffda6e" 
+        />
+        <CompanionCard
+           id="456"
+           name="Countsy the Number Wizard"
+           topic="Derivatives & Integrals"
+           subject="Maths"
+           duration={30}
+           color="#e5d0ff" 
+        />
+        <CompanionCard
+           id="789"
+           name="Verba the Vocabulary Builder"
+           topic="English Literature"
+           subject="English"
+           duration={30}
+           color="#bde7ff" 
+        />
+        
+      </section>
+
+      <section className="home-section">
+        <ComapnionLists
+          title="Recently completed sessions"
+          companions={recentSessions}
+          classNames="w-2/3 max-lg:w-full" 
+        />
+        <CTA />
+      </section>
+    </main>
   )
 }
 
